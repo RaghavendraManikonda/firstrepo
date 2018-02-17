c82bf6f6097b753d7b8b2953e700cc52b72d72aa
RaghavendraManikonda <raghav.live365@gmail.com>
2018-02-17 17:33:18 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "STARTING SECOND PROJECT FROM GIT PLUGIN"
+echo "#######################################"
+mkdir /var/tmp/secondproject
+cd /var/tmp/secondproject
+touch one
+touch two
+touch three
+echo "End of second project"
